ccdd407273cedf1d536e227a229f16b47ac727b3
Charles Adu Boakye <4cyberlord@proton.me>
2023-01-21 14:04:50 +0000

    Changing internal design via code after view loads

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,9 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // IBOutlet allows me to connect to a storyboard view.
+    
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    
+    @IBOutlet weak var diceImageTwo: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Who           What     Value.
+        // Chaning our internal design value after loading. 
+        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix")
+        
+        diceImageViewOne.alpha = 0.5
+        
+        diceImageTwo.image = UIImage(imageLiteralResourceName: "DiceTwo")
         // Do any additional setup after loading the view.
     }
 
