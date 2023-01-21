b93b540d92fb6a8e6dfb4535e6a5ff3863e38aa3
Charles Adu Boakye <4cyberlord@proton.me>
2023-01-21 18:35:38 +0000

    Programatically changing the images

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -16,26 +16,36 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var diceImageTwo: UIImageView!
     
+    
+    var leftDiceNumber = 0
+    var rightDiceNumber = 5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Who           What     Value.
-        // Chaning our internal design value after loading.
-        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix")
-        
-        diceImageViewOne.alpha = 0.5
-        
-        diceImageTwo.image = UIImage(imageLiteralResourceName: "DiceTwo")
+//        // Who           What     Value.
+//        // Chaning our internal design value after loading.
+//        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix")
+//
+//        diceImageViewOne.alpha = 0.5
+//
+//        diceImageTwo.image = UIImage(imageLiteralResourceName: "DiceTwo")
         
         // Do any additional setup after loading the view.
     }
-
-  
+    
+    
     @IBAction func rowButtonPressed(_ sender: Any) {
-        //        print("Button got tapped")
-                diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceFour")
-                diceImageTwo.image = UIImage(imageLiteralResourceName: "DiceFour")
+        
+        diceImageViewOne.image = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix"),][leftDiceNumber];
+        
+        diceImageTwo.image =  [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix"),][rightDiceNumber];
+        
+        
+        leftDiceNumber = leftDiceNumber + 1
+        rightDiceNumber = rightDiceNumber - 1
+        
+        
     }
     
 }
-
