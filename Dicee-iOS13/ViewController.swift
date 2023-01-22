6d9c07806effc58e4955c01350e5761e01b084f1
Charles Adu Boakye <4cyberlord@proton.me>
2023-01-22 01:15:41 +0000

    Constrains and layout implementation applied

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -20,30 +20,23 @@ class ViewController: UIViewController {
     var leftDiceNumber = 0
     var rightDiceNumber = 5
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-//        // Who           What     Value.
-//        // Chaning our internal design value after loading.
-//        diceImageViewOne.image = UIImage(imageLiteralResourceName: "DiceSix")
-//
-//        diceImageViewOne.alpha = 0.5
-//
-//        diceImageTwo.image = UIImage(imageLiteralResourceName: "DiceTwo")
-        
-        // Do any additional setup after loading the view.
-    }
     
     
     @IBAction func rowButtonPressed(_ sender: Any) {
         
-        diceImageViewOne.image = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix"),][leftDiceNumber];
+        let diceArray = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix"),];
         
-        diceImageTwo.image =  [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"),UIImage(imageLiteralResourceName: "DiceThree"),UIImage(imageLiteralResourceName: "DiceFour"),UIImage(imageLiteralResourceName: "DiceFive"),UIImage(imageLiteralResourceName: "DiceSix"),][rightDiceNumber];
+//        diceImageViewOne.image = diceArray[Int.random(in: 0...5)];
+        diceImageViewOne.image = diceArray.randomElement()
+        
+//        diceImageTwo.image =  diceArray[Int.random(in: 0...5)];
+        diceImageTwo.image = diceArray.randomElement()
         
         
-        leftDiceNumber = leftDiceNumber + 1
-        rightDiceNumber = rightDiceNumber - 1
+        leftDiceNumber = leftDiceNumber + 1;
+        rightDiceNumber = rightDiceNumber - 1;
+        
+//        print(Int.random(in: 1...10))
         
         
     }
